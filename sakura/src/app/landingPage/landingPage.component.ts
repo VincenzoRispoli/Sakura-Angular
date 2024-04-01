3d113abc1130c884fb3412d8e9944bf5a718ca32
vincenzorispoli.vr@gmail.com <you@example.com>
2024-04-01 22:45:04 +0200

    new components

---
+++ sakura/src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: 'app-landingPage', // il selettore é il tag HTML che identifica il componente. Questo ci permette di inserire questo componente all'interno di alri componenti tramite il tag <app-landingPage></app-landingPage>
+    standalone: true, // con questa proprietá su true, diciao che il componente é a se stante;
+    imports: [CommonModule], // la proprietá imports ci permette di importare altri componenti o servizi come quelli che sono in alto "CommonModule etc"
+    templateUrl: './landingPage.component.html', // questa prorpietá puo chiamarsi template dove all'interno scriviamo il codice HTML direttamente, oppure puó chiamarsi templateUrl, dove assegniamo il percorso del file html del componente
+    styleUrls: ['./landingPage.component.scss']
+})
+export class LandingPageComponent {   // per rendere questa classe sempre accesibile, anche dagli altri componenti si usa export
+
+}
