7a6081e2d1f9bc0b2a74ce81020739800aa343e9
vincenzorispoli.vr@gmail.com <you@example.com>
2024-04-02 22:49:11 +0200

    new components created

---
+++ sakura/src/app/main-content/landingPage/landingPage.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: 'app-landingPage',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './landingPage.component.html',
+    styleUrl: './landingPage.component.scss',
+
+})
+
+export class LangingPageComponent {
+
+}
