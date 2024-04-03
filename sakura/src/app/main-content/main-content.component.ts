f8f3d9fedbe0d92bf88361d7bc3f6a0b305dc889
vincenzorispoli.vr@gmail.com <you@example.com>
2024-04-03 18:15:48 +0200

    new component

--- sakura/src/app/main-content/main-content.component.ts
+++ sakura/src/app/main-content/main-content.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LangingPageComponent } from './landingPage/landingPage.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
+import { OurMenuComponent } from './our-menu/our-menu.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, LangingPageComponent, HowToOrderComponent, OurMenuComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
