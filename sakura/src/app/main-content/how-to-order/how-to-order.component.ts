64f59a4abf19c50d6260579f7f710889cf97151d
vincenzorispoli.vr@gmail.com <you@example.com>
2024-04-03 23:08:09 +0200

    new components

--- sakura/src/app/main-content/how-to-order/how-to-order.component.ts
+++ sakura/src/app/main-content/how-to-order/how-to-order.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-how-to-order',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './how-to-order.component.html',
   styleUrl: './how-to-order.component.scss'
 })
